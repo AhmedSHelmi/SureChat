4b696a9800e1f5cbc6f8ca3d571cbc24762acb9d
Ahmed Helmi <ahmed.shawkat@protonmail.com>
2019-09-11 14:16:22 +0200

    server ip axios

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://54.246.132.23/'
-axios.defaults.baseURL = 'http://localhost/'
+axios.defaults.baseURL = 'http://3.19.229.198/'
+// axios.defaults.baseURL = 'http://localhost/'
 
 Vue.config.productionTip = false
 
